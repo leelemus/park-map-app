ac771e4d206d1e401167c33e3193ddfa8ded60d0
Lee Lemus <leelemus@gmail.com>
2019-06-25 20:54:35 -0500

    added locate button and funcionality

--- main.js
+++ main.js
@@ -1,3 +1,77 @@
 'use strict';
 
-const gmapiKey = "AIzaSyC5kUVY_9QNtKnDzOlq3StWxkEZICDMLb8";
+/*
+
+function findLocation() {
+    // HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+    }, 
+        function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+*/
+
+function findLocation() {
+    // HTML5 geolocation.
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+  
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+    });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
+function locateDevice() {
+    $('.contentContainer').on('click',`.iconButton`, event => {
+        console.log('this works');
+        findLocation();
+    });
+}
+
+function mapApp() {
+    locateDevice ();
+}
+
+$(mapApp);
+
+
+
