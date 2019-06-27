0aa36b726cdf852df36c6af518f4b565cc04045c
Lee Lemus <leelemus@gmail.com>
2019-06-26 22:56:15 -0500

    got routing to work and have stops displayed on screen.

--- main.js
+++ main.js
@@ -1,66 +1,57 @@
 'use strict';
 
-/*
-
-var request = {
-    location: pyrmont,
-    radius: '500',
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-
-
-*/
-
 var map;
-var infoWindow;
 var service;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 43.074409, lng: -89.384085},
-    zoom: 15
-  });
-  infoWindow = new google.maps.InfoWindow;
-  var bikeLayer = new google.maps.BicyclingLayer();
-  bikeLayer.setMap(map);
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 43.074409, lng: -89.384085},
+        zoom: 15
     });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+
+    let infoWindow = new google.maps.InfoWindow;
+
+    let bikeLayer = new google.maps.BicyclingLayer();
+    bikeLayer.setMap(map);
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                let pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, 
+            function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
 
 function findLocation() {
     // HTML5 geolocation.
     navigator.geolocation.getCurrentPosition(function(position) {
 
-        var pos = {
+        let pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
@@ -74,68 +65,16 @@ function findLocation() {
 
 function locateDevice() {
     $('.js-contentContainer').on('click',`.js-iconButton`, event => {
-        console.log('this works');
         findLocation();
-    });
-}
-
-/*
-function getResults() {
-    $('.js-createRouteForm').on('click',`#createRouteButton`, event => {
-
-        event.preventDefault();  
-        var pyrmont = new google.maps.LatLng(43.074409, -89.384085);
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pyrmont,
-            zoom: 15
-        });
-
-        var request = {
-            location: pyrmont,
-            query: 'playground',
-            radius: '500'
-        };
-
-        service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
-    });
-}
-*/
-
-function getResults() {
-    $('.js-createRouteForm').on('click',`#createRouteButton`, event => {
-        event.preventDefault(); 
-        // Try HTML5 geolocation.
-        
-            navigator.geolocation.getCurrentPosition(function(position) {
-            
-                let searchLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: searchLocation,
-                    zoom: 15
-                });
-                var bikeLayer = new google.maps.BicyclingLayer();
-                bikeLayer.setMap(map);
-                let request = {
-                    location: searchLocation,
-                    radius: '1000',
-                    keyword: 'park playground',
-                    type: ['park']
-                  };
-                let service = new google.maps.places.PlacesService(map);
-                  service.nearbySearch(request, callback);
-            });
     });
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (let i = 0; i < results.length; i++) {
-        console.log(results.length);
-      createMarker(results[i]);
-    }
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay, results);
   }
 }
 
@@ -151,9 +90,78 @@ function createMarker(place) {
     });
 }
 
+function calculateAndDisplayRoute(directionsService, directionsDisplay, results) {
+    let waypts = [];
+    let checkboxArray = results;
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        for (let i = 0; i < checkboxArray.length; i++) {
+            if (checkboxArray[i].place_id) {
+              waypts.push({
+                location: {
+                  'placeId': checkboxArray[i].place_id
+                },
+                stopover: true
+              });
+            }
+          }
+
+        directionsService.route({
+            origin: pos,
+            destination: pos,
+            waypoints: waypts,
+            optimizeWaypoints: true,
+            travelMode: 'WALKING'
+          }, function(response, status) {
+            if (status === 'OK') {
+              directionsDisplay.setDirections(response);
+              var route = response.routes[0];
+              var summaryPanel = document.getElementById('directions-panel');
+              summaryPanel.innerHTML = '';
+              // For each route, display summary information.
+              for (var i = 0; i < route.legs.length; i++) {
+                var routeSegment = i + 1;
+                summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                    '</b><br>';
+                summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+              }
+            } else {
+              window.alert('Directions request failed due to ' + status);
+            }
+          });
+    });
+  }
+
+function generateRoute() {
+    $('.js-createRouteForm').on('click',`#createRouteButton`, event => {
+        event.preventDefault(); 
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            let searchLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    
+            let request = {
+                location: searchLocation,
+                radius: '1000',
+                keyword: 'park playground',
+                type: ['park']
+            };
+    
+            let service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request, callback);
+        });
+    });
+}
+
 function mapApp() {
     locateDevice ();
-    getResults();
+    generateRoute();
 }
 
 $(mapApp);
