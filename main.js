20941eec638d95fab49198621dbaa3bac422566f
Lee Lemus <leelemus@gmail.com>
2019-06-26 23:01:42 -0500

    added one finger gesture handling and two finger zoom

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 43.074409, lng: -89.384085},
-        zoom: 15
+        zoom: 15,
+        gestureHandling: 'cooperative'
     });
 
     let infoWindow = new google.maps.InfoWindow;
